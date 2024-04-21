5cb2aa60351b0fe339adea1e9336dc8cfaf87a8a
Vlad <71086815+swixge@users.noreply.github.com>
2024-04-21 11:56:59 +0300

    start
    start

---
+++ Search for RiPD/Search for RiPD/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Search_for_RiPD
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
